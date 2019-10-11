489629cd4d8a8d48a47508b6dae1b19fdb5939f2
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-11 00:39:58 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+  if speed < 40 || speed > 60
+    return true 
+  else speed >= 41 || speed <= 59
+    return false  
+  end 
 end
+
+unsafe?(50)
 
 
 
 def not_safe?(speed)
-	
+  speed < 40 || speed > 60 ? true : false
 end
 	
-
 

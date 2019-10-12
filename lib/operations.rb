c2c4be9f07ba4dbee7c3f5ab97a39e89a42f0156
Wendy Harris <wendyv510@hotmail.com>
2019-10-12 18:14:42 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+    if speed <= 40 || speed >=60
+      puts "unsafe"
+    else speed >40 && <60
+      puts "safe" 
+    
 end
 
 
 
 def not_safe?(speed)
-	
+	40>=(speed)>=60  ?  "safe" : "unsafe" 
 end
 	
 
